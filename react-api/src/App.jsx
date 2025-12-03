a32c900448f4964f931c4a14afc7d4558e437f28
AlexanderYepez-code <alexsaomn17@gmail.com>
2025-12-03 17:19:17 +0100

    feat: stampata la lsita delle atrici e procedo con la grafica

--- react-api/src/App.jsx
+++ react-api/src/App.jsx
@@ -1,20 +1,44 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import axios from "axios";
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [personaggi, setpersonaggi] = useState([]);
 
+useEffect(() =>{
+  listActors()
+}, [])
 
-
-  function listActors(){
-    axios.get('https://lanciweb.github.io/demo/api/actresses/').then((list)=>{
-      console.log(resp)
+  function listActors() {
+    axios.get('https://lanciweb.github.io/demo/api/actresses/').then((list) => {
+      setpersonaggi(list.data)
     })
   }
-  // return (
+listActors()
+  return (
+    <section>
+    <h1>Lista di atrici</h1>
     
-  // )
+    <div>
+      {personaggi.map((personaggio, )=>
+      <div key={personaggio.id}>
+        <h2>{personaggio.name}</h2>
+        <img src={personaggio.image} alt="" />
+        <p>{personaggio.birth_year}         {personaggio.nationality}</p>
+        <p>{personaggio.biography}</p>
+        <p>Known For:{personaggio.most_famous_movies}</p>
+        <p>Awards:{personaggio.awards}</p>
+
+      </div>
+
+      
+      )}
+
+    </div>
+
+
+    </section>
+  )
 }
 
 export default App
